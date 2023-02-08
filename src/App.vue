9182ec8c0a7be6e01c40d76cdb4674e7d792dc66
Beom Mee <whitebigbabybird@gmail.com>
2023-02-08 22:53:44 +0900

    Add Login view to the app page and link the auth store

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router'
+import { onMounted } from "vue";
+import { storeToRefs } from "pinia";
+import { useAuthStore } from "@/stores/auth";
+import LoginView from "@/views/LoginView.vue";
+
+const store = useAuthStore();
+const { auth } = storeToRefs(store);
+
+onMounted(() => {
+  console.log(auth.value.isLoggedIn);
+});
 </script>
 
 <template>
-  <RouterView />
+  <!-- <RouterView /> -->
+  <LoginView v-if="!auth.value?.isLoggedIn" />
 </template>
 
 <style scoped></style>
